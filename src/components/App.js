8b728ea2ca929cd3afa4068653c9639dc83b1c18
koskei-kipkoech <patrickwayy@gmail.com>
2024-11-04 19:49:14 +0300

    updates src/components

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,24 @@
+import { useEffect, useState } from "react";
+
 // create your App component here
+function App(){
+    const [dogImage, setDogImage]= useState(null)
+    useEffect(() =>{
+        fetch("https://dog.ceo/api/breeds/image/random")
+            .then((response) => response.json())
+            .then((data) => setDogImage(data.message))
+            //.catch((error) => console.log('Error Fetching dog image:',error))
+    },[])
+    
+    return(
+        <div>
+            {dogImage ? (
+                <img src={dogImage} alt="A Random Dog"/>
+            ): (
+                <p>Loading...</p>
+            )}
+        </div>
+    );
+}
+
+export default App;
